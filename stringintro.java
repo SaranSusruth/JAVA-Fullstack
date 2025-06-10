551baaceaef8a833a3a394ad8d8c5ea1954c50fe
SaranSusruth <contactsusruth@gmail.com>
2025-06-10 15:34:35 +0530

    string introduction and some methods

---
+++ stringintro.java
@@ -0,0 +1,10 @@
+public class stringintro 
+{
+  public static void main(String[] a)
+  {
+    String name = new String("harsh");
+    String name2 = "rewant";
+    System.out.println(name.length());
+    System.out.println(name2.charAt(3));
+  }
+}
