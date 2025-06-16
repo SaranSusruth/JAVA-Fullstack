70d4677def2bd8f389d1f228354b28ffb1c27d3f
SaranSusruth <contactsusruth@gmail.com>
2025-06-16 11:04:44 +0530

    this is a program for Quiz game

--- Quiz-Console/QuestionService.java
+++ Quiz-Console/QuestionService.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class QuestionService
- {
-     Question questions[] = new Question[5];
-     String userAnswer[] = new String[5];
+{
+    Question[] questions = new Question[5];
+    String[] userAnswer = new String[5];
 
-     QuestionService()
-     {
-        questions[0]= new Question(1,"what is size of int ","1","2","4","8", "3");
-        questions[1]= new Question(2,"what is size of char ","1","2","4","8", "1");
-        questions[2] = new Question(3,"what is size of float ","1","2","4","8", "3");
-        questions[3] = new Question(4,"what is size of double ","1","2","4","8", "4");
-        questions[4] = new Question(5,"what is size of long ","1","2","4","8", "4");
-     }
+    public QuestionService()
+    {
+        questions[0] = new Question(1, "what is size of int ", "1", "2", "4", "8", "3");
+        questions[1] = new Question(2, "what is size of char ", "1", "2", "4", "8", "1");
+        questions[2] = new Question(3, "what is size of float ", "1", "2", "4", "8", "3");
+        questions[3] = new Question(4, "what is size of double ", "1", "2", "4", "8", "4");
+        questions[4] = new Question(5, "what is size of long ", "1", "2", "4", "8", "4");
+    }
 
-     public void playQuiz()
-     {
+    public void playQuiz()
+    {
+        Scanner sc = new Scanner(System.in);
         int i = 0;
         for (Question q : questions)
         {
@@ -25,20 +26,20 @@ public class QuestionService
             System.out.println("2. " + q.getOpt2());
             System.out.println("3. " + q.getOpt3());
             System.out.println("4. " + q.getOpt4());
-            Scanner sc = new Scanner(System.in);
             userAnswer[i] = sc.nextLine();
             i++;
-         }
-         System.out.println("Your answers are :");
-         for ( String ans : userAnswer)
-         {
+        }
+        sc.close();
+        System.out.println("Your answers are :");
+        for (String ans : userAnswer)
+        {
             System.out.println(ans);
-         }
-     }
+        }
+    }
 
-     public void getScore()
-     {
-        int score  = 0;
+    public void getScore()
+    {
+        int score = 0;
         for (int i = 0; i < questions.length; i++)
         {
             if (userAnswer[i].equals(questions[i].getAnswer()))
@@ -47,7 +48,5 @@ public class QuestionService
             }
         }
         System.out.println("Your score is : " + score + " out of " + questions.length);
-     }
-     
-
- }
+    }
+}
