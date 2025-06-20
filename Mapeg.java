4c3b58d8c5b1e2f0e788ba4dec552c4d7bce22e8
SaranSusruth <contactsusruth@gmail.com>
2025-06-20 16:48:47 +0530

    this is a example program on Maps data structure

--- Mapeg.java
+++ Mapeg.java
@@ -6,15 +6,22 @@ public class Mapeg
     public static void main(String[] a)
     {
         Map<String,Integer> stud = new HashMap<String,Integer>();
-
+        //adding elements
         stud.put("Nikhil",21);
         stud.put("Susruth",18);
         stud.put("Bhavani",23);
         stud.put("Sai",24);
+        stud.put("Anand",24);
+        //geting one value of a particular key
+        System.out.println(stud.get("Sai"));
+        //getting the all keys
+        System.out.println(stud.keySet());
 
-        System.out.println(stud);
+        for (String key : stud.keySet())
+        {
+            System.out.println(key + ":" + stud.get(key));
+        }
 
-         System.out.println(stud.get("Sai"));
     }
     
 }
